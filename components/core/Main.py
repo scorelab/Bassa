2cac1e90cb3be272c6f51cb6b8481616b79eeebe
Milindu Sanoj Kumarage <agentmilindu@gmail.com>
2015-05-08 13:18:24 +0530

    Gitignore added, Main.py restructured  On branch py-flask 	new file:   .gitignore 	modified:   Main.py

--- components/core/Main.py
+++ components/core/Main.py
@@ -1,15 +1,10 @@
 #!flask/bin/python
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return "Hello, World!"
+from REST import *
 
 
 def main():
-	app.run(debug=True)
-	
+    server.run(debug=True)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    main()
+
