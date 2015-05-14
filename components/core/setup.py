b4983b80195ea55f02f0f9299a2a4ff1b1900da1
Milindu Sanoj Kumarage <agentmilindu@gmail.com>
2015-05-14 09:18:42 +0530

    improved formatting, removed unused install requirements

--- components/core/setup.py
+++ components/core/setup.py
@@ -3,25 +3,25 @@ from setuptools import setup
 
 
 # Utility function to read the README file.
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+def read(file_name):
+    return open(os.path.join(os.path.dirname(__file__), file_name)).read()
 
 setup(
-    name = "bassa",
-    version = "0.0.1",
-    author = "SCoRe Community",
-    author_email = "community@scorelab.org",
-    description = ("Automated Download Queue for Enterprise to take the best use of Internet bandwidth"),
-    license = "GPL",
-    keywords = "bassa download queue",
-    url = "https://github.com/scorelab/Bassa",
+    name="bassa",
+    version="0.0.1",
+    author="SCoRe Community",
+    author_email="community@scorelab.org",
+    description="Automated Download Queue for Enterprise to take the best use of Internet bandwidth",
+    license="GPL",
+    keywords="bassa download queue",
+    url="https://github.com/scorelab/Bassa",
     packages=['tests'],
-    install_requires=['flask', 'WorQ[redis]','pip','pySmartDL'],
+    install_requires=['flask', 'WorQ[redis]', 'pySmartDL'],
     long_description=read('README.md'),
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
-	"Framework :: Flask"
+        "Framework :: Flask",
         "License :: OSI Approved :: GNU General Public License (GPL)",
     ],
     entry_points={
