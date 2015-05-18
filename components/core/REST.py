1596032e48578805ebc25d8f796baec96a00d73e
paarandika <paarandika@gmail.com>
2015-05-18 10:30:15 -0700

    Added RQ function route

--- components/core/REST.py
+++ components/core/REST.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask import request
 from worq import get_queue
-
+from redis import Redis
+from rq import Queue
+from func import downloadMe
 
 server = Flask(__name__)
 q = get_queue("memory://")
-
+redis_conn = Redis()
+qu = Queue(connection=redis_conn)
 
 @server.route('/')
 def index():
@@ -18,4 +21,10 @@ def download():
     #r = q.tasks.add(2)
     #print(r)
     q.tasks.download(link, destination)
+    return '{ "msg": "Added to the queue"}'
+
+@server.route('/api/download1',  methods=['GET'])
+def download1():
+    link = request.args.get('link', '')
+    job = qu.enqueue(downloadMe,link)
     return '{ "msg": "Added to the queue"}'
