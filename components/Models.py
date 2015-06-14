7d8e708040ac88bbeb463ff3cafc86bd57519b03
paarandika <paarandika@gmail.com>
2015-06-14 05:40:20 -0700

    Added login function

---
+++ components/Models.py
@@ -0,0 +1,6 @@
+class User:
+    def __init__(self, userName, password, auth, token):
+        self.userName=userName
+        self.password=password
+        self.auth=auth
+        self.token=token
