aedd9e1003ca5d2a13ad863831649999f02fe89e
paarandika <paarandika@gmail.com>
2015-08-01 02:56:26 -0700

    Updated DB file

--- Bassa.sql
+++ Bassa.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 14, 2015 at 02:00 PM
--- Server version: 5.5.43-0ubuntu0.14.04.1
--- PHP Version: 5.5.9-1ubuntu4.9
+-- Generation Time: Aug 01, 2015 at 02:55 AM
+-- Server version: 5.5.44-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -19,32 +19,44 @@ SET time_zone = "+00:00";
 --
 -- Database: `Bassa`
 --
-CREATE DATABASE IF NOT EXISTS `Bassa` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `Bassa`;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Table structure for table `download`
 --
 
-CREATE TABLE IF NOT EXISTS `user` (
+CREATE TABLE IF NOT EXISTS `download` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `link` text NOT NULL,
   `user_name` varchar(256) NOT NULL,
-  `password` varchar(256) NOT NULL,
-  `auth` tinyint(11) NOT NULL,
-  `email` varchar(256) NOT NULL,
-  `blocked` tinyint(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`user_name`),
-  UNIQUE KEY `user_name` (`user_name`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `added_time` bigint(20) NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '0',
+  `rating` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  KEY `user_name` (`user_name`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
 
 --
--- Dumping data for table `user`
+-- Dumping data for table `download`
 --
 
-INSERT INTO `user` (`user_name`, `password`, `auth`, `email`, `blocked`) VALUES
-('rand', '1a1dc91c907325c69271ddf0c944bc72', 0, 'rand@mail.com', 0);
+INSERT INTO `download` (`id`, `link`, `user_name`, `added_time`, `status`, `rating`) VALUES
+(6, 'eyJhbGciOiJIUzI1NiIsImV4cCI6MTQzODM0NjU4NywiaWF0IjoxNDM4MzQ1OTg3fQ.eyJ1c2VyTmFtZSI6InJhbmQifQ.OzkhGRHBQX8u3xuqZ2lI9rvMWv_vqQ7T3-lnSsAGNJk', 'rand', 1438363998, 3, 0),
+(7, 'eyJhbGciOiJIUzI1NiIsImV4cCI6MTQzODM0NjU4NywiaWF0IjoxNDM4MzQ1OTg3fQ.eyJ1c2VyTmFtZSI6InJhbmQifQ.OzkhGRHBQX8u3xuqZ2lI9rvMWv_vqQ7T3-lnSsAGNJk', 'rand', 1438364004, 0, 0),
+(8, 'eyJhbGciOiJIUzI1NiIsImV4cCI6MTQzODM0NjU4NywiaWF0IjoxNDM4MzQ1OTg3fQ.eyJ1c2VyTmFtZSI6InJhbmQifQ.OzkhGRHBQX8u3xuqZ2lI9rvMWv_vqQ7T3-lnSsAGNJk', 'rand', 1438364005, 3, 3),
+(9, 'eyJhbGciOiJIUzI1NiIsImV4cCI6MTQzODM0NjU4NywiaWF0IjoxNDM4MzQ1OTg3fQ.eyJ1c2VyTmFtZSI6InJhbmQifQ.OzkhGRHBQX8u3xuqZ2lI9rvMWv_vqQ7T3-lnSsAGNJk', 'rand', 1438364006, 0, 0);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `download`
+--
+ALTER TABLE `download`
+  ADD CONSTRAINT `download_ibfk_1` FOREIGN KEY (`user_name`) REFERENCES `user` (`user_name`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
