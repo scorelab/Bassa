5c51e1b485929093bd4cf2ec4506876108913336
paarandika <paarandika@gmail.com>
2015-08-05 15:39:08 -0700

    email util

--- components/core/EMail.py
+++ components/core/EMail.py
@@ -1,1 +1,10 @@
-__author__ = 'rand'
+import smtplib
+
+username = 'bassa.ucsc@gmail.com'
+password = 'bassa.123'
+
+def send_mail(toaddrs, msg):
+    server = smtplib.SMTP_SSL('smtp.gmail.com:465')
+    server.login(username,password)
+    server.sendmail('bassa.ucsc@gmail.com', toaddrs, msg)
+    server.quit()
