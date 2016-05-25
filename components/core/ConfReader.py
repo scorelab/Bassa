5b540c51193b3526cb9d44b2a8858e137585a30b
Malith Senaweera <malithsenaweera@gmail.com>
2016-05-25 21:09:39 +0530

    Warn when conf files aren't set properly

--- components/core/ConfReader.py
+++ components/core/ConfReader.py
@@ -1,6 +1,20 @@
 import os
 import inspect
 import json
+import sys
+
+def check_conf_availability():
+    """Checks whether the conf files have been properly set. Exits otherwise"""
+    dlConf = get_conf_reader('dl.conf')
+    emailConf = get_conf_reader('email.conf')
+
+    if any(val == '' for val in dlConf.values()):
+        sys.exit("Please set all the values in dl.conf")
+    if any(val == '' for val in emailConf.values()):
+        sys.exit("Please set all the values in email.conf")
+
+    dlConf.close()
+    emailConf.close()
 
 def get_conf_reader(confFile):
     f = open(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))) + "/" + confFile)
