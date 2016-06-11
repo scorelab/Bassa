b72c3ab1c51e2828ef313a20c1fcf14127cafa8d
Malith Senaweera <malithsenaweera@gmail.com>
2016-06-11 10:48:55 +0530

    Restrict access to routes if not authenticated

--- ui/app/js/app.js
+++ ui/app/js/app.js
@@ -11,11 +11,23 @@ angular.module('bassa', ['ui.router', 'ngMaterial'])
     .state('app', {
       url: "/app",
       templateUrl: "views/home.html",
-      controller: "RootCtrl"
+      controller: "RootCtrl",
+      authenticate: true
     })
     .state('login', {
       url: "/login",
       templateUrl: "views/login.html",
-      controller: "LoginCtrl"
+      controller: "LoginCtrl",
+      authenticate: false
     });
+})
+
+.run(function ($rootScope, $state, Security) {
+  $rootScope.$on("$stateChangeStart", function(event, toState, toParams, fromState, fromParams){
+    if (toState.authenticate && !Security.loggedIn()) {
+      // User isn’t authenticated
+      $state.transitionTo("login");
+      event.preventDefault();
+    }
+  });
 });
