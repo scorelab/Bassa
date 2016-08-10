b091b59536958f2125717528bdca3cdec188a957
Malith Senaweera <malithsenaweera@gmail.com>
2016-08-10 15:46:21 +0530

    Admin Controller unit tests

---
+++ ui/src/app/admin/controllers/AdminCtrl.spec.js
@@ -0,0 +1,77 @@
+describe('Controller: Admin controller', function() {
+  beforeEach(module('bassa'))
+  beforeEach(module('app'));
+
+  var scope, AdminCtrl, BassaUrl;
+
+  var fakeHttpPromise = function () {
+    return {
+      then: function () {
+        return true;
+      }
+    };
+  };
+
+  beforeEach(inject(function($controller, $rootScope) {
+
+    scope = $rootScope.$new();
+    AdminCtrl = $controller('AdminCtrl', {
+       '$scope': scope
+    });
+
+  }));
+
+  it('should have AdminCtrl defined', function() {
+      expect(AdminCtrl).toBeDefined();
+  });
+
+  it('start should call AdminService.startDownloads()', inject(function (AdminService) {
+
+    spyOn(AdminService, 'startDownloads').and.callFake(fakeHttpPromise);
+  
+    scope.start();
+  
+    expect(AdminService.startDownloads).toHaveBeenCalled();
+  
+  }));
+
+  it('kill should call AdminService.killDownloads()', inject(function (AdminService) {
+
+    spyOn(AdminService, 'killDownloads').and.callFake(fakeHttpPromise);
+  
+    scope.kill();
+  
+    expect(AdminService.killDownloads).toHaveBeenCalled();
+  
+  }));
+
+  it('approve should call AdminService.approve()', inject(function (AdminService) {
+
+    spyOn(AdminService, 'approve').and.callFake(fakeHttpPromise);
+  
+    scope.approve();
+  
+    expect(AdminService.approve).toHaveBeenCalled();
+  
+  }));
+
+  it('getRequests should call AdminService.getSignupRequests()', inject(function (AdminService) {
+
+    spyOn(AdminService, 'getSignupRequests').and.callFake(fakeHttpPromise);
+  
+    AdminCtrl.getRequests();
+  
+    expect(AdminService.getSignupRequests).toHaveBeenCalled();
+  
+  }));
+
+  it('getHeavyUsers should call AdminService.getHeavyUsers()', inject(function (AdminService) {
+    
+    spyOn(AdminService, 'getHeavyUsers').and.callFake(fakeHttpPromise);
+  
+    AdminCtrl.getHeavyUsers();
+  
+    expect(AdminService.getHeavyUsers).toHaveBeenCalled();
+  
+  }));
+});
