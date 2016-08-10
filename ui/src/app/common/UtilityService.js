66f0d7a97bb5f4bce7d21ceb2db3d75327a7e6a8
Malith Senaweera <malithsenaweera@gmail.com>
2016-08-10 10:08:26 +0530

    Calculate file size only upto 3 decimal places

--- ui/src/app/common/UtilityService.js
+++ ui/src/app/common/UtilityService.js
@@ -6,11 +6,11 @@
 
   function UtilityService(){
 
-    var formatBytes = function(bytes, decimals) {
+    var formatBytes = function(bytes) {
 
       if(bytes == 0) return '0 Byte';
       var k = 1000,
-          dm = decimals + 1 || 3,
+          dm = 3,
           sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
           i = Math.floor(Math.log(bytes) / Math.log(k));
       return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
