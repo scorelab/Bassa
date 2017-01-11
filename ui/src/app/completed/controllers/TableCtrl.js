6d3582de19093511bd9b894bf6ab5cfafd02a673
Jeffrey Kam <jeffreykam0415@gmail.com>
2017-01-12 03:08:32 +0800

    fixing use strict mistakes

--- ui/src/app/completed/controllers/TableCtrl.js
+++ ui/src/app/completed/controllers/TableCtrl.js
@@ -1,5 +1,5 @@
 (function(){
-
+  'use strict';
   angular
     .module('app')
     .controller('TableCtrl', [ '$scope', 'ToastService', 'TableService', 'UtilityService', TableCtrl]);
--- ui/src/app/controllers/MessagesController.js
+++ ui/src/app/controllers/MessagesController.js
@@ -1,5 +1,5 @@
 (function(){
-
+  'use strict';
   angular
     .module('app')
     .controller('MessagesController', [
