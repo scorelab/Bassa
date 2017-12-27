36d6270d655901676321dcd126525bfc25f7ec53
padamchopra <jb.padamchopra@gmail.com>
2017-12-27 23:26:30 +0530

    Included suggestions

--- scripts/init-database.py
+++ scripts/init-database.py
@@ -1,28 +1,43 @@
 #!/usr/bin/env python
 
-import yaml, sqlalchemy, MySQLdb
+import yaml
+import sqlalchemy
+import MySQLdb
 
-stream = open("../.config/bassa/config.yml", "r")
-docs = yaml.load_all(stream)
 values = list()
-for doc in docs:
-    for k,v in doc.items():
-        for m,n in v.items():
-		values.append(n)
-dbtype=values[5]
-dbusername=values[0]
-dbpass=values[4]
-ip=values[2]
-# db=MySQLdb.connect(host="localhost", user="bassaman",passwd="s57d46857f968t79pho")
-# db1=db.cursor()
-# db1.execute('Create database test100')
-engine = sqlalchemy.create_engine(dbtype+'://'+dbusername+':'+dbpass+'@'+ip)
-engine.execute("CREATE DATABASE IF NOT EXISTS " + values[3])
-engine = sqlalchemy.create_engine(dbtype+'://'+dbusername+':'+dbpass+'@'+ip+'/'+values[3])
-fd = open("../Bassa.sql",'r')
-sqlFile = fd.read()
-fd.close()
-sqlCommands=sqlFile.split(';')
-for command in sqlCommands:
-	with engine.connect() as con:
-		rs = con.execute(command+';')
+
+
+def retreiveValues():
+    stream = open("../.config/bassa/config.yml", "r")
+    docs = yaml.load_all(stream)
+    global values
+    for doc in docs:
+        for k, v in doc.items():
+            for m, n in v.items():
+                values.append(n)
+
+
+def createDatabase():
+    param = values[5]+'://'+values[0]+':'+values[4]+'@'+values[2]
+    engine = sqlalchemy.create_engine(param)
+    engine.execute("CREATE DATABASE IF NOT EXISTS " + values[3])
+
+
+def importSQL():
+    param = values[5]+'://'+values[0]+':'+values[4]+'@'+values[2]+'/'+values[3]
+    engine = sqlalchemy.create_engine(param)
+    fd = open("../Bassa.sql", 'r')
+    sqlFile = fd.read()
+    fd.close()
+    sqlCommands = sqlFile.split(';')
+    for command in sqlCommands:
+        try:
+            with engine.connect() as con:
+                con.execute(command+';')
+        except:
+            error = 1
+retreiveValues()
+createDatabase()
+importSQL()
+print "Database (" + values[3] + ") is now successfuly setup"
+
