d38ab6cb4604bef2903e87c80b24d66efe3a0f7f
Moses Paul R <iammosespaulr@gmail.com>
2018-01-16 19:10:44 +0530

    Added Login Tests as well

--- test/test_Bassa_endpoint.py
+++ test/test_Bassa_endpoint.py
@@ -1,6 +1,19 @@
 import unittest
+from flask_api import FlaskAPI
 import requests
 
+headers = {
+'Host': 'localhost:5000',
+'Accept': 'application/json, text/plain, */*',
+'Accept-Language': 'en-US,en;q=0.5',
+'Accept-Encoding': 'gzip, deflate',
+'Referer': 'http://localhost:3000/',
+'Content-Type': 'application/x-www-form-urlencoded',
+'Content-Length': '28',
+'Origin': 'http://localhost:3000',
+'Connection': 'close'
+}
+payload = {''}
 class TestFlaskAPIUsingRequests(unittest.TestCase):
     def test_api_for_404(self):
         response = requests.get('http://localhost:5000')
@@ -10,6 +23,12 @@ class TestFlaskAPIUsingRequests(unittest.TestCase):
         response = requests.get('http://localhost:5000/api')
         print "code:",response.status_code
         self.assertEqual(response.status_code,404)
-
+    def test_api_login(self):
+        resp = requests.post('http://localhost:5000/api/login',"user_name=rand&password=pass",headers=headers)
+        print(resp.status_code)
+	print(resp.json())
+        self.assertEqual(resp.json(),{u'auth': u'0'})
 if __name__ == "__main__":
     unittest.main()
+			
+			
