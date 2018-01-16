48df931a17be0a32b66abc4d8759ad42fb205548
Moses Paul R <iammosespaulr@gmail.com>
2018-01-16 20:12:23 +0530

    Update test_Bassa_endpoint.py
    Changed the test according to the requests..

--- test/test_Bassa_endpoint.py
+++ test/test_Bassa_endpoint.py
@@ -6,30 +6,23 @@ headers = {
 'Accept': 'application/json, text/plain, */*',
 'Accept-Language': 'en-US,en;q=0.5',
 'Accept-Encoding': 'gzip, deflate',
-'Referer': 'http://localhost:3000/',
 'Content-Type': 'application/x-www-form-urlencoded',
-'Content-Length': '28',
-'Origin': 'http://localhost:3000',
-'Connection': 'close'
+'Origin': 'http://localhost:3000'
 }
+correct_username="rand"
+correct_password="pass"
+incorrect_username="admin"
+incorrect_password="admin"
+correct_string="user_name="+correct_username+"&password="+correct_password
+incorrect_string="user_name="+incorrect_username+"&password="+incorrect_password
 payload = {''}
 class TestFlaskAPIUsingRequests(unittest.TestCase):
-    def test_api_for_404(self):
-        response = requests.get('http://localhost:5000')
-        print "code:",response.status_code
-        self.assertEqual(response.status_code,404)
-    def test_api_path_for_404(self):
-        response = requests.get('http://localhost:5000/api')
-        print "code:",response.status_code
-        self.assertEqual(response.status_code,404)
-    def test_api_login(self):
-        resp = requests.post('http://localhost:5000/api/login',"user_name=rand&password=pass",headers=headers)
-        print(resp.status_code)
-	print(resp.json())
-        self.assertEqual(resp.json(),{u'auth': u'0'})
-    def test_api_login_incorrectly(self):
-        resp = requests.post('http://localhost:5000/api/login',"user_name=rand&password=paass",headers=headers)
-        print(resp.status_code)
+    def test_api_login_returns_auth_level(self):
+        resp = requests.post('http://localhost:5000/api/login',correct_string,headers=headers)
+        def check_valid_auth(self):
+         self.assertEqual(resp.json(),{u'auth': u'0'})
+    def test_api_login_incorrectly_return_403(self):
+        resp = requests.post('http://localhost:5000/api/login',incorrect_string,headers=headers)
         self.assertEqual(resp.status_code,403)
 if __name__ == "__main__":
     unittest.main()
