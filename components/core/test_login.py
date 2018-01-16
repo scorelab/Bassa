5821a7b887f60a1f223916a81e6c33f4ab40b8f4
Moses Paul R <iammosespaulr@gmail.com>
2018-01-16 12:52:32 +0530

    Update and rename Test.py to test_login.py
    Adding functions to implement functions as docstrings did not work ... @malithsen please review

---
+++ components/core/test_login.py
@@ -0,0 +1,20 @@
+from UserManager import * 
+import unittest
+def username(): 
+ return "admin"
+def password(): 
+ return "admin"
+def usernamer(): 
+ return "rand"
+def passwordr(): 
+ return "pass"
+class Test(unittest.TestCase):
+ def test_Incorrect_Login(self):
+    self.assertEqual(False, user_login(username(), password()))
+ def test_Correct_Login(self):
+    self.assertEqual(True, user_login(usernamer(), passwordr()))
+ def test_Incorrect_check_approved(self):
+    self.assertEqual(False, check_approved(username(), password()))
+ def test_Correct_check_approved(self):
+    self.assertEqual(True, check_approved(usernamer(), passwordr()))
+unittest.main()  
