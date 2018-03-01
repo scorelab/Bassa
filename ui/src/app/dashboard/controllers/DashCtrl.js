16138bb8eb46bbb98d5cfe90ead1e94e5197dfb4
Nirmal Sarswat <nirmalsarswat400@gmail.com>
2018-03-02 02:20:32 +0530

    using in-built dialog box for showing prompts
    Signed-off-by: Nirmal Sarswat <nirmalsarswat400@gmail.com>

--- ui/src/app/dashboard/controllers/DashCtrl.js
+++ ui/src/app/dashboard/controllers/DashCtrl.js
@@ -2,18 +2,18 @@
   'use strict';
   angular
     .module('app')
-    .controller('DashCtrl', [ '$scope','$window','ToastService', 'DashService', 'UserService', 'BassaUrl', DashCtrl]);
-
-  function DashCtrl($scope, $window , ToastService, DashService, UserService, BassaUrl) {
+    .controller('DashCtrl', [ '$scope','$window','ToastService', 'DashService', 'UserService', 'BassaUrl','$mdDialog', DashCtrl]);
+  
+  function DashCtrl($scope, $window , ToastService, DashService, UserService, BassaUrl, $mdDialog) {
     var socket = io.connect(BassaUrl + '/progress');
     $scope.dlink = {link: ''};
     $scope.downloads = [];
     $scope.username = UserService.getUsername();
-
+    
     socket.on('connect', function(){
       socket.emit('join', {room: $scope.username});
     });
-
+    
     socket.on('status', function(data) {
       _.forEach($scope.downloads, function(obj){
         if (obj.id == data.id) {
@@ -22,18 +22,13 @@
         }
       });
     });
-
+    
     var linkvalidator = function(link){
       var urlvalidator =/^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/[^\s]*)?$/i;
       var magnetvalidator = /magnet:\?xt=/i;
-      if (urlvalidator.test(link) || link.match(magnetvalidator) !== null){
-        return true;
-      }
-      else{
-        return false;
-        }
-    }
-
+      return urlvalidator.test(link) || link.match(magnetvalidator) !== null;
+    };
+    
     $scope.addLink = function() {
       if ($scope.dlink.link === '' || $scope.dlink.link === undefined || !linkvalidator($scope.dlink.link)) {
         ToastService.showToast('Please check your url');
@@ -57,29 +52,37 @@
         var data = response.data;
         $scope.downloads = _.filter(data, function(d) {return d.status==0});
         $scope.downloads = _.map($scope.downloads, function(element) {
-             return _.extend({}, element, {progress: 0});
+          return _.extend({}, element, {progress: 0});
         });
       }, function(error){
         ToastService.showToast("Oops! Something went wrong when fetching data");
       });
-    }
-
+    };
     $scope.removeLink = function (id) {
       // Shows window confirmation before deleting download.
-      var deleteDownload = $window.confirm('Confirm download task deletion !');
-
-      if(deleteDownload){
-        DashService.removeDownload(id).then(function(response){
-          ToastService.showToast("Download removed");
-          getActiveDownloads();},
-          function (error){
-          ToastService.showToast("Download started. Entry cannot be deleted.");
+      // Using angular in-built dialog box
+      
+      var confirm = $mdDialog.confirm()
+        .title(' Do you want to stop the download')
+        .textContent('You can\'t start this download back.\n')
+        .ariaLabel('Stopping download')
+        .ok('Yes')
+        .cancel('No');
+      
+      $mdDialog.show(confirm).then(function() {
+          DashService.removeDownload(id).then(function(response){
+              ToastService.showToast("Download removed");
+              getActiveDownloads();},
+            function (error){
+              ToastService.showToast("Download started. Entry cannot be deleted.");
+            })},
+        function (){
+        
         });
-      }
-     };
-
+    };
+    
     getActiveDownloads();
     
   }
-
+  
 })();
