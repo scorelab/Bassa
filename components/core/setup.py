b371898eea5fb611e1028663bd9812eab5f150f6
Vishrut Kohli <vishrutkohli@Vishruts-MacBook-Air.local>
2018-03-11 00:20:33 +0530

    updated withpep8 style

--- components/core/setup.py
+++ components/core/setup.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import os
 import shutil
 import inspect
@@ -9,11 +11,15 @@ EMAIL_CONF = 'email.conf'
 DL_CONF = 'dl.conf'
 LINUX_CONFDIR = os.path.expanduser('~') + '/.config/bassa/'
 WIN_CONFDIR = os.path.expanduser('~') + '/%app_data%/bassa/'
-OSX_CONFDIR  = os.path.expanduser('~') + '/.config/bassa/'
+OSX_CONFDIR = os.path.expanduser('~') + '/.config/bassa/'
+
 
 # Utility function to read the README file.
+
 def read(file_name):
-    return open(os.path.join(os.path.dirname(__file__), file_name)).read()
+    return open(os.path.join(os.path.dirname(__file__),
+                file_name)).read()
+
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
 requirements_path = os.path.join(base_dir, 'requirements.txt')
@@ -22,7 +28,8 @@ install_reqs = parse_requirements(requirements_path, session=False)
 
 requirements = [str(ir.req) for ir in install_reqs]
 
-### Set configs ###
+# ### Set configs ###
+
 if platform.system() == 'Linux':
     configdir = LINUX_CONFDIR
 elif platform.system() == 'Windows':
@@ -32,34 +39,33 @@ elif platform.system() == 'Darwin':
 if not os.path.exists(configdir):
     os.makedirs(configdir)
 
-email_conf_location = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))) + "/" + EMAIL_CONF
-dl_conf_location = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))) + "/" + DL_CONF
+email_conf_location = \
+    os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))) \
+    + '/' + EMAIL_CONF
+dl_conf_location = \
+    os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))) \
+    + '/' + DL_CONF
 shutil.copyfile(email_conf_location, configdir + EMAIL_CONF)
 shutil.copyfile(dl_conf_location, configdir + DL_CONF)
 
-###/ Set configs ###
+# ###/ Set configs ###
 
 setup(
-    name="bassa",
-    version="0.0.1",
-    author="SCoRe Community",
-    author_email="community@scorelab.org",
-    description="Automated Download Queue for Enterprise to take the best use of Internet bandwidth",
-    license="GPL",
-    keywords="bassa download queue",
-    url="https://github.com/scorelab/Bassa",
+    name='bassa',
+    version='0.0.1',
+    author='SCoRe Community',
+    author_email='community@scorelab.org',
+    description='Automated Download Queue for Enterprise to ' +
+    'take the best use of Internet bandwidth',
+    license='GPL',
+    keywords='bassa download queue',
+    url='https://github.com/scorelab/Bassa',
     packages=['tests'],
     install_requires=requirements,
     long_description=read('README.md'),
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Topic :: Utilities",
-        "Framework :: Flask",
-        "License :: OSI Approved :: GNU General Public License (GPL)",
-    ],
-    entry_points={
-        'console_scripts': [
-            'bassa = Main:main'
-        ]
-    },
-)
+    classifiers=['Development Status :: 3 - Alpha', 'Topic :: Utilities',
+                 'Framework :: Flask',
+                 'License :: OSI Approved :: GNU General Public License (GPL)'
+                 ],
+    entry_points={'console_scripts': ['bassa = Main:main']},
+    )
