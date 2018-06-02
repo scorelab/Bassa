dba6ae14c468747e139f8da99de4a8aa9fcf3852
Nirmal Sarswat <nirmalsarswat400@gmail.com>
2018-06-02 19:15:06 +0530

    add authorization to request

--- components/core/file_sender/sender.py
+++ components/core/file_sender/sender.py
@@ -5,21 +5,24 @@ from Main import logging
 
 @server.route("/api/get/file", methods=['GET'])
 def send_file_from_path():
-    # TODO :: Add Authorization to the request
-    try:
-        gid = request.args.get('gid')
-        logging.info('User asked for file, having GID ' + gid)
-        id = get_id_from_gid(gid)
-        download_path = get_download_path(id)
-        download_name = get_download_name_from_id(id)
-    except Exception as e:
-        logging.error(" get file API (/api/get/file) got wrong arguments, thrown an error >> " + str(e) + " <<")
-        return Response("error", status=200)
+    token = token_validator(request.args['token'])
+    if token is not None:
+        try:
+            gid = request.args.get('gid')
+            logging.info('User asked for file, having GID ' + gid)
+            id = get_id_from_gid(gid)
+            download_path = get_download_path(id)
+            download_name = get_download_name_from_id(id)
+        except Exception as e:
+            logging.error(" get file API (/api/get/file) got wrong arguments, thrown an error >> " + str(e) + " <<")
+            return Response("error", status=200)
 
-    try:
-        return send_file(filename_or_fp=download_path, attachment_filename=download_name,
-                         as_attachment=True)
-    except Exception as e:
-        logging.error("File sending has a exception. We got >> "+str(e)+" << error on file sending")
-        return Response("File you are trying to access is not available to us. Please ask admin to check the server",
-                        status=200)
+        try:
+            return send_file(filename_or_fp=download_path, attachment_filename=download_name,
+                             as_attachment=True)
+        except Exception as e:
+            logging.error("File sending has a exception. We got >> "+str(e)+" << error on file sending")
+            return Response("File you are trying to access is not available to us. Please ask admin to check the server"
+                            , status=200)
+    else:
+        return Response("Invalid Token in request", 403)
