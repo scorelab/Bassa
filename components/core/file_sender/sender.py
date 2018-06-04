7e1e67c217663704b06747e4bbe1202010017271
Nirmal Sarswat <nirmalsarswat400@gmail.com>
2018-06-05 01:40:31 +0530

    formatted log messages
    Signed-off-by: Nirmal Sarswat <nirmalsarswat400@gmail.com>

--- components/core/file_sender/sender.py
+++ components/core/file_sender/sender.py
@@ -13,13 +13,13 @@ def send_file_from_path():
         download_path = get_download_path(id)
         download_name = get_download_name_from_id(id)
     except Exception as e:
-        logging.error(" get file API (/api/get/file) got wrong arguments, thrown an error >> " + str(e) + " <<")
+        logging.error(" get file API (/api/get/file) got wrong arguments, thrown an error :: %s" % e)
         return Response("error", status=200)
 
     try:
         return send_file(filename_or_fp=download_path, attachment_filename=download_name,
                          as_attachment=True)
     except Exception as e:
-        logging.error("File sending has a exception. We got >> "+str(e)+" << error on file sending")
+        logging.error("File sending has a exception. We got :: %s on file sending" % e)
         return Response("File you are trying to access is not available to us. Please ask admin to check the server",
                         status=200)
