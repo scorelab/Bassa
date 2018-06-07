2ec40f1c09317fffc9c24a24860143e280a105ca
Nirmal Sarswat <nirmalsarswat400@gmail.com>
2018-06-07 12:58:11 +0530

    changed code according to the reviews
    Signed-off-by: Nirmal Sarswat <nirmalsarswat400@gmail.com>

--- ui/src/app/completed/controllers/TableCtrl.js
+++ ui/src/app/completed/controllers/TableCtrl.js
@@ -6,51 +6,40 @@
 
   function TableCtrl($scope, ToastService, TableService, UtilityService) {
     $scope.downloads = [];
+    $scope.isGridVisible = false;
     var setSize = function(lst) {
       lst.data.forEach(function(download) {
         download.size = UtilityService.formatBytes(download.size);
-      })
+      });
       return lst;
     };
-    $scope.isGridOn = false;
     TableService.getCompletedDownloads().then(function (response) {
       response = setSize(response);
       $scope.downloads = response.data;
     }, function(error){
       ToastService.showToast('Oops! Something went wrong fetching data');
     });
-    $scope.changeView = function () {
-        if($scope.isGridOn) {
-            document.getElementById('grid-button').innerText = 'view_module';
-            $scope.isGridOn = false;
-        }else{
-            document.getElementById('grid-button').innerText = 'view_list';
-            $scope.isGridOn = true;
-        }
-    };
     $scope.getImage = function (fileName) {
-        var fileExtensionName = getExtension(fileName);
+      var fileExtensionName = getExtension(fileName);
       return "../../../assets/images/file-images/"+fileExtensionName+".png";
     };
+
     function getExtension(fileName){
-        var fileExtension = fileName.split('.');
-        var fileExtensionName ;
-        if(fileExtension){
-            fileExtensionName = fileExtension[fileExtension.length-1];
-            if(!isExtensionMatch(fileExtensionName)){
-                fileExtensionName = '_page'
-            }
-        }else{
-            fileExtensionName = '_page'
+      var fileExtension = fileName.split('.');
+      var fileExtensionName ;
+      if(fileExtension){
+        fileExtensionName = fileExtension[fileExtension.length-1];
+        if(!isExtensionMatch(fileExtensionName)){
+          fileExtensionName = '_page';
         }
-        return fileExtensionName;
+      }else{
+        fileExtensionName = '_page';
+      }
+      return fileExtensionName;
     }
 
     function isExtensionMatch(extensionName) {
-        var checkExtension = fileTypeArray.indexOf(extensionName);
-        return checkExtension !== -1;
+      return fileTypeArray.indexOf(extensionName) !== -1;
     }
-
   }
-
 })();
