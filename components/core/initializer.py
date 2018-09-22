64c2b06de4e24f9bc8f790a267c74eee8af20303
Nirmal Sarswat <nirmalsarswat400@gmail.com>
2018-09-22 19:18:32 +0530

    Update initializer.py

--- components/core/initializer.py
+++ components/core/initializer.py
@@ -4,12 +4,12 @@ from flask_cors import CORS
 from flask_socketio import SocketIO
 from gevent import monkey
 
-from utils.app_constants import server_secret_key
+from utils.app_constants import SERVER_SECRET_KEY
 
 monkey.patch_all(ssl=False)
 
 server = Flask(__name__)
-server.config['SECRET_KEY'] = server_secret_key
+server.config['SECRET_KEY'] = SERVER_SECRET_KEY
 socketio = SocketIO(server, debug=True, logger=True, engineio_logger=True, ping_timeout=600)
 cors = CORS(server)
 p = None
