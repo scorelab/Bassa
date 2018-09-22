65377ed5dcd8217b65d559186c28615c12b3add5
Nirmal Sarswat <nirmalsarswat400@gmail.com>
2018-09-22 19:16:52 +0530

    Update token_utils.py

--- components/core/utils/token_utils.py
+++ components/core/utils/token_utils.py
@@ -1,12 +1,13 @@
 from flask import g
 from Auth import verify_auth_token, generate_auth_token
-from utils.app_constants import server_secret_key
+from utils.app_constants import SERVER_SECRET_KEY
+
 
 
 def token_validator(token):
-    user = verify_auth_token(token, server_secret_key)
+    user = verify_auth_token(token, SERVER_SECRET_KEY)
     if user is not None:
         g.user = user
-        token = generate_auth_token(user, server_secret_key)
+        token = generate_auth_token(user, SERVER_SECRET_KEY)
         return token
     return None
