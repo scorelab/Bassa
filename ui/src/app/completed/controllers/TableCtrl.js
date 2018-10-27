3962f401081551573a6608b803fb7884c79d3066
anuragparida <anuragparida37@gmail.com>
2018-10-28 03:05:11 +0530

    Add dropdown menu of details to Bassa completed section

--- ui/src/app/completed/controllers/TableCtrl.js
+++ ui/src/app/completed/controllers/TableCtrl.js
@@ -1,26 +1,35 @@
-(function(){
-  'use strict';
+(function() {
+  "use strict";
   angular
-    .module('app')
-    .controller('TableCtrl', [ '$scope', 'ToastService', 'TableService', 'UtilityService', TableCtrl]);
+    .module("app")
+    .controller("TableCtrl", [
+      "$scope",
+      "ToastService",
+      "TableService",
+      "UtilityService",
+      TableCtrl
+    ]);
 
   function TableCtrl($scope, ToastService, TableService, UtilityService) {
     $scope.downloads = [];
-
+    $scope.myFunction = function(idd) {
+      document.getElementById(idd).classList.toggle("show");
+    };
     var setSize = function(lst) {
       lst.data.forEach(function(download) {
         download.size = UtilityService.formatBytes(download.size);
-      })
+      });
       return lst;
     };
 
-    TableService.getCompletedDownloads().then(function (response) {
-      response = setSize(response);
-      $scope.downloads = response.data;
-    }, function(error){
-      ToastService.showToast('Oops! Something went wrong fetching data');
-    });
-
+    TableService.getCompletedDownloads().then(
+      function(response) {
+        response = setSize(response);
+        $scope.downloads = response.data;
+      },
+      function(error) {
+        ToastService.showToast("Oops! Something went wrong fetching data");
+      }
+    );
   }
-
 })();
