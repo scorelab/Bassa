ef44b491002c1531b49378bc04efc89caf8419f3
avsingh999 <201651011@iiitvadodara.ac.in>
2019-03-26 16:20:03 +0530

    improve login section for short screen size

--- components/core/DBCon.py
+++ components/core/DBCon.py
@@ -5,9 +5,10 @@ _db=None
 
 def get_db_con () :
     global _db
+    print("###")
     if _db==None:
         try:
-            _db=MySQLdb.connect("db", os.environ.get('BASSA_DB_USERNAME'), os.environ.get('BASSA_DB_PASSWORD'), os.environ.get('BASSA_DB_NAME'))
+            _db=MySQLdb.connect(host="localhost", user="root", passwd="password",db= "Bassa")
             return _db
         except:
             return None
