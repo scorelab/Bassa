4e4c6fe20f9179a4067eda4af80e5dbd70a1f1a3
VibhorCodecianGupta <guptavibhu98@gmail.com>
2019-06-15 09:41:59 +0530

    project helpers

--- components/core/DirectoryManager.py
+++ components/core/DirectoryManager.py
@@ -7,7 +7,10 @@ conf = get_conf_reader("dl.conf")
 
 threadpool = pool.QueuePool(get_db_con, max_overflow=10, pool_size=20)
 
-def get_all_workspaces(user_id):
+
+##################### workspace helper functions ######################
+
+def get_workspaces(user_id):
     db = threadpool.connect()
     if db is not None:
         cursor = db.cursor()
@@ -21,7 +24,7 @@ def get_all_workspaces(user_id):
         return 'success'
     return 'db connection error'
 
-def get_workspace_by_id(user_id, workspace_id):
+def get_workspace_by_id(workspace_id, user_id):
     db = threadpool.connect()
     if db is not None:
         cursor = db.cursor()
@@ -49,13 +52,72 @@ def create_workspace(name, user_id):
         return 'success'
     return 'db connection error'
 
-def update_workspace(name, user_id):
+def update_workspace(name, id, user_id):
     db = threadpool.connect()
     if db is not None:
         cursor = db.cursor()
-        sql = "UPDATE workspace SET name=%s where user_id=%d;"
+        sql = "UPDATE workspace SET name=%s WHERE id=%d AND user_id=%d;"
         try:
-            cursor.execute(sql, (name, user_id))
+            cursor.execute(sql, (name, id, user_id))
+            db.commit()
+        except MySQLdb.error as e:
+            db.rollback()
+            return e[1]
+        return 'success'
+    return 'db connection error'
+
+
+##################### project helper functions ######################
+
+def get_projects(workspace_id):
+    db = threadpool.connect()
+    if db is not None:
+        cursor = db.cursor()
+        sql = "SELECT name FROM project WHERE workspace_id=%d;"
+        try:
+            cursor.execute(sql, (workspace_id))
+            db.commit()
+        except MySQLdb.error as e:
+            db.rollback()
+            return e[1]
+        return 'success'
+    return 'db connection error'
+
+def get_project_by_id(project_id, workspace_id):
+    db = threadpool.connect()
+    if db is not None:
+        cursor = db.cursor()
+        sql = "SELECT name FROM project WHERE id=%d AND workspace_id=%d;"
+        try:
+            cursor.execute(sql, (project_id, workspace_id))
+            db.commit()
+        except MySQLdb.error as e:
+            db.rollback()
+            return e[1]
+        return 'success'
+    return 'db connection error'
+
+def create_project(name, workspace_id):
+    db = threadpool.connect()
+    if db is not None:
+        cursor = db.cursor()
+        sql = "INSERT into project VALUES(%s, %d);"
+        try:
+            cursor.execute(sql, (name, workspace_id))
+            db.commit()
+        except MySQLdb.error as e:
+            db.rollback()
+            return e[1]
+        return 'success'
+    return 'db connection error'
+
+def update_project(name, id, workspace_id):
+    db = threadpool.connect()
+    if db is not None:
+        cursor = db.cursor()
+        sql = "UPDATE project SET name=%s where id=%d AND workspace_id=%d;"
+        try:
+            cursor.execute(sql, (name, id, workspace_id))
             db.commit()
         except MySQLdb.error as e:
             db.rollback()
