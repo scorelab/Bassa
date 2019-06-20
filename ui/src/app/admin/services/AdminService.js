c6fff6e4aafe457672cbbdc1c93d9fd0a78e5736
Pawan Bhadu <31997208+demon-36@users.noreply.github.com>
2019-06-20 13:05:23 +0530

    Update AdminService.js

--- ui/src/app/admin/services/AdminService.js
+++ ui/src/app/admin/services/AdminService.js
@@ -43,11 +43,19 @@
       });
     }
 
+    var visitMinio = function() {
+      var url = 'http://192.168.43.195:9000'
+      setTimeout(function(){
+        window.open(url, '_blank');
+         },1100)
+    };
+
     return {
       startDownloads : startDownloads,
       killDownloads : killDownloads,
       getSignupRequests : getSignupRequests,
       approve: approve,
+      visitMinio : visitMinio,
       getHeavyUsers: getHeavyUsers
     };
   }
