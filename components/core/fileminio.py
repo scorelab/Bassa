d60807c2f9231a2a4973d03268baeda5344e6fdb
Pawan Bhadu <31997208+demon-36@users.noreply.github.com>
2019-06-22 22:26:36 +0530

    Update fileminio.py

--- components/core/fileminio.py
+++ components/core/fileminio.py
@@ -2,15 +2,39 @@ from flask import Response, request
 from DownloadDaemon import *
 from minio import Minio
 from minio.error import ResponseError
+from DownloadManager import *
+from DBCon import *
+import sys
+import sqlalchemy.pool as pool
+
+client = Minio('localhost:9000', secure=False,
+               access_key='bassa',
+                  secret_key='bassa123')
 
 def upload_to_minio(file_name, file_path):
-	client = Minio('localhost:9000', secure=False,
-               access_key='2N2XFGY92ML8JMXGB2M4',
-                  secret_key='a6Nsx+lRtj3Cm9lxO1JW3xqHXG+sIKmunNIl1U7V')
 	fname = file_name
 	fpath = file_path
 	try:
 		client.fput_object('bassa', fname, fpath)
 	except ResponseError as err:
 		print(err)
-	
+
+
+def update_minio_indexes(file_name, file_path, gid, completedLength, username, download_id):
+	db = threadpool.connect()
+	if db is not None:
+		cursor = db.cursor()
+		m_fname = file_name
+		m_fpath = file_path
+		m_id = download_id
+		m_gid = gid
+		m_size = completedLength
+		m_username = username
+		sql = "INSERT into minio(mid, mgid, muser_name, mfile_name, msize, mpath) VALUES(%s, %s, %s, %s, %s, %s);"
+		try:
+			cursor.execute(sql, (m_id, m_gid, m_username, m_fname, m_size, m_fpath))
+			db.commit()
+		except MySQLdb.Error as e:
+			db.rollback()
+			# Shows error thrown up by database
+			print(e)
