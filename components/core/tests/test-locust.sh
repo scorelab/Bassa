0f397c935b369f812749f64cae4e05714437430b
Joseph Semrai <josephsemrai@gmail.com>
2019-12-08 03:34:03 -0500

    Update automated locust test script

---
+++ components/core/tests/test-locust.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo Installing locustio
+python3 -m pip install locustio
+
+start_locust(){
+    echo Starting Locust... Locust will not be ready to use until the Docker container starts.
+    locust
+    if [ $? -ne 0 ]; then
+    echo "Error launching Locust. There may be something wrong with your Python install."
+    fi
+}
+
+start_docker(){
+    echo Building and running container
+    echo Building from: $PWD...
+    docker-compose -f ../../../docker-compose.yml up --build
+    if [ $? -ne 0 ]; then
+        echo "Error launching docker-compose. You may not have docker/docker-compose installed. Please refer to: https://docs.docker.com/install"
+    fi
+}
+
+start_locust &
+start_docker
