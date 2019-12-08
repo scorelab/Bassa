2d60120434a3ff0507801a7f10cb0b428ff5ff96
Joseph Semrai <josephsemrai@gmail.com>
2019-12-08 03:59:58 -0500

    Styling/Codacy fix

--- components/core/tests/locustfile.py
+++ components/core/tests/locustfile.py
@@ -1,11 +1,6 @@
 from locust import HttpLocust, TaskSequence, task, seq_task, between
-from random import randint
-
-USERNAME = "rand"
-PASSWORD = "pass"
 
 class UserBehavior(TaskSequence):
-    
     headers = {
         'Content-Type': 'application/x-www-form-urlencoded',
     }
@@ -14,36 +9,36 @@ class UserBehavior(TaskSequence):
         self.login()
 
     def login(self):
-        res = self.client.post("/api/login", f"user_name={USERNAME}&password={PASSWORD}", headers=self.headers)
+        res = self.client.post("/api/login", "user_name=rand&password=pass", headers=self.headers)
         self.headers['token'] = res.headers['token']
 
     @seq_task(1)
     @task(1)
-    def userRequests(self):
+    def user_requests(self):
         self.client.get("/api/user/requests", headers=self.headers)
 
     @seq_task(2)
     @task(1)
-    def userBlocked(self):
+    def user_blocked(self):
         self.client.get("/api/user/blocked", headers=self.headers)
 
     @seq_task(3)
     @task(3)
-    def userDownloads(self):
+    def user_downloads(self):
         downloadEndpoints = [25, 50, 75]
         for endpoint in downloadEndpoints:
             self.client.get("/api/user/downloads/" + str(endpoint), headers=self.headers)
 
     @seq_task(4)
     @task(2)
-    def getDownloadsRequest(self):
+    def get_downloads_request(self):
         requestEndpoints = [25, 50, 75]
         for endpoint in requestEndpoints:
             self.client.get("/api/downloads/" + str(endpoint), headers=self.headers)
 
     @seq_task(5)
     @task(3)
-    def getUserHeavy(self):
+    def get_user_heavy(self):
         self.client.get("/api/user/heavy", headers=self.headers)
 
 class APIUser(HttpLocust):
