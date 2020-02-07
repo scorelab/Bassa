f9708619d4ba43f8ecc711970da08eb8f1f5380c
hv7214 <hverma1@cs.iitr.ac.in>
2020-02-08 00:40:40 +0530

    doc(wiki/*): added db config system guide Note: This should be appended to wiki

--- components/core/DBCon.py
+++ components/core/DBCon.py
@@ -3,13 +3,13 @@ from ConfReader import get_conf_reader
 import os
 
 _db=None
-dbConf = get_conf_reader("db.config.json")
+db_conf = get_conf_reader("db.config.json")
 
 def get_db_con () :
     global _db
     if _db==None:
         try:
-            _db=MySQLdb.connect(host=dbConf["BASSA_HOST"], user=dbConf["BASSA_DB_USERNAME"], passwd=dbConf["BASSA_DB_PASSWORD"],db=dbConf["BASSA_DB_NAME"])
+            _db=MySQLdb.connect(host=db_conf["BASSA_HOST"], user=db_conf["BASSA_DB_USERNAME"], passwd=db_conf["BASSA_DB_PASSWORD"],db=db_conf["BASSA_DB_NAME"])
             return _db
         except:
             return None
