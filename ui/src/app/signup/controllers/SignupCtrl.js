b602f7014e1d979087a5beb01f2010c38e5da976
Sahan Dilshan <32576163+Amstrom@users.noreply.github.com>
2020-03-14 10:48:06 +0530

    Add register with strong password feature

--- ui/src/app/signup/controllers/SignupCtrl.js
+++ ui/src/app/signup/controllers/SignupCtrl.js
@@ -46,6 +46,37 @@
         ToastService.showToast('Please ensure entered details are correct');
       }
     };
-  };
+
+
+    function passwordValid() {
+      $scope.loginForm.password.$valid = true;
+      $scope.loginForm.password.$dirty = false;
+      $scope.loginForm.password.$invalid = false;
+    }
+    function passwordInValid() {
+      $scope.loginForm.password.$valid = false;
+      $scope.loginForm.password.$invalid = true;
+      $scope.loginForm.password.$dirty = true;
+    }
+
+    $scope.validatePwd = function () {
+      let lowerCaseLetters = /[a-z]/g;
+      let uppserCaseLetters = /[A-Z]/g;
+      let numbers = /[0-9]/g;
+      if (typeof $scope.user.password !== 'undefined') {
+        if (!$scope.user.password.match(lowerCaseLetters)) {
+          passwordInValid();
+        } if (!$scope.user.password.match(uppserCaseLetters)) {
+          passwordInValid();
+        } else if (!$scope.user.password.match(numbers)) {
+          passwordInValid();
+        } else if ($scope.user.password.length < 8) {
+          passwordInValid();
+        } else {
+          passwordValid();
+        }
+      }
+    };
+  }
 
 })();
