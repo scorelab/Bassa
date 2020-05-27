6514c431f5fed9442436271febfc99bcef45a23c
K mehant <kmehant@gmail.com>
2020-05-28 02:22:46 +0530

    Refactor Code

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,27 @@
 #! /bin/bash
 
 # Build production and development use docker images
-# Build these images with two build args for better commit history 
+# Build these images with two build args for better commit history
 # shows only logs pushed to stderr
-docker build --build-arg VCS_REF=`git rev-parse --short HEAD`   --build-arg BUILD_DATE=`date -u +”%Y-%m-%dT%H:%M:%SZ”` -t scoreucsc/bassa-prod-ui ui >/dev/null
-docker build -f ui/Dockerfile.dev --build-arg VCS_REF=`git rev-parse --short HEAD`   --build-arg BUILD_DATE=`date -u +”%Y-%m-%dT%H:%M:%SZ”` -t scoreucsc/bassa-dev-ui ui >/dev/null
-docker build -f components/core/Dockerfile.prod --build-arg VCS_REF=`git rev-parse --short HEAD`   --build-arg BUILD_DATE=`date -u +”%Y-%m-%dT%H:%M:%SZ”` -t scoreucsc/bassa-prod-server components/core >/dev/null
-docker build --build-arg VCS_REF=`git rev-parse --short HEAD`   --build-arg BUILD_DATE=`date -u +”%Y-%m-%dT%H:%M:%SZ”` -t scoreucsc/bassa-dev-server components/core >/dev/null
-docker build --build-arg VCS_REF=`git rev-parse --short HEAD`   --build-arg BUILD_DATE=`date -u +”%Y-%m-%dT%H:%M:%SZ”` -t scoreucsc/bassa-aria2c components/aria2c >/dev/null
+# release tag should be updated here for every release
+git_sha=$(git rev-parse --short HEAD)
+build_date=$(date -u +”%Y-%m-%dT%H:%M:%SZ”)
+docker build --build-arg VCS_REF=$git_sha --build-arg BUILD_DATE=$build_date -t scoreucsc/bassa-ui:prod -t scoreucsc/bassa-ui:latest -t scoreucsc/bassa-ui:v1.0.0  ui >/dev/null
+docker build -f ui/Dockerfile.dev --build-arg VCS_REF=$git_sha --build-arg BUILD_DATE=$build_date -t scoreucsc/bassa-ui:dev ui >/dev/null
+docker build -f components/core/Dockerfile.prod --build-arg VCS_REF=$git_sha --build-arg BUILD_DATE=$build_date -t scoreucsc/bassa-server:prod -t scoreucsc/bassa-server:latest -t scoreucsc/bassa-server:v1.0.0 components/core >/dev/null
+docker build --build-arg VCS_REF=$git_sha --build-arg BUILD_DATE=$build_date -t scoreucsc/bassa-server:dev components/core >/dev/null
+docker build --build-arg VCS_REF=$git_sha --build-arg BUILD_DATE=$build_date -t scoreucsc/bassa-aria2c:latest -t scoreucsc/bassa-aria2c:v1.0.0 components/aria2c >/dev/null
 
-# push both production and development use docker images to dockerhub
+# push production docker images with tags prod, latest, release:v1.0.0
+# push development docker images with tag dev
 docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
-docker push scoreucsc/bassa-prod-ui
-docker push scoreucsc/bassa-dev-ui
-docker push scoreucsc/bassa-prod-server
-docker push scoreucsc/bassa-dev-server
-docker push scoreucsc/bassa-aria2c
+docker push scoreucsc/bassa-ui:prod
+docker push scoreucsc/bassa-ui:latest
+docker push scoreucsc/bassa-ui:v1.0.0
+docker push scoreucsc/bassa-ui:dev
+docker push scoreucsc/bassa-server:prod
+docker push scoreucsc/bassa-server:latest
+docker push scoreucsc/bassa-server:v1.0.0
+docker push scoreucsc/bassa-server:dev
+docker push scoreucsc/bassa-aria2c:latest
+docker push scoreucsc/bassa-aria2c:v1.0.0
