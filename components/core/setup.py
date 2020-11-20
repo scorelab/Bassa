ad2bf0b2beb91c64b277d8203c8185c05bd00cc7
apg360 <53176930+apg360@users.noreply.github.com>
2020-11-20 21:38:51 +0200

    Fix error in setup.py  No module named 'pip.req'
    Fix the error as seen here : https://stackoverflow.com/questions/25192794/no-module-named-pip-req
    
    The Error :
    # python3 setup.py develop
    Traceback (most recent call last):
      File "setup.py", line 6, in <module>
        from pip.req import parse_requirements
    ImportError: No module named 'pip.req'

--- components/core/setup.py
+++ components/core/setup.py
@@ -3,7 +3,10 @@ import shutil
 import inspect
 import platform
 from setuptools import setup
-from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 EMAIL_CONF = 'email.conf'
 DL_CONF = 'dl.conf'
